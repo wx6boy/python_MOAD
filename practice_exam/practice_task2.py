0c2864a1a08b40f8bcb6d7790bddc7899ff32275
wx6boy <a.klenkov52@mail.ru>
2022-12-28 16:52:19 +0300

    added examples to task 2

--- practice_exam/practice_task2.py
+++ practice_exam/practice_task2.py
@@ -1,15 +1,15 @@
 def read_file(filename):
     f = open(filename, "r", encoding="utf-8")
     data = f.read()
+    data = data.replace("\n", " ")
     f.close()
 
     return data
 
 
-def write_file(filename, sentences_list):
+def write_file(filename, string_to_write):
     with open(filename, "w") as file:
-        for line in sentences_list:
-            file.write(line + '\n')
+        file.write(string_to_write)
 
 
 def get_beginning_values():
@@ -17,15 +17,13 @@ def get_beginning_values():
 
 
 def preprocess_data(data: str, max_len: int):
-    data = data.replace("\n", "")
     data_list = data.split()
 
     result_list = []
     current_string, current_len = get_beginning_values()
     for current_word in data_list:
-        if current_len + len(current_word) > max_len:
+        if current_len + len(current_word) + 1 > max_len:
             result_list.append(current_string)
-            current_string, current_len = get_beginning_values()
             current_string = current_word
             current_len = len(current_word)
         else:
@@ -36,13 +34,13 @@ def preprocess_data(data: str, max_len: int):
                 current_string = current_word
                 current_len = len(current_word)
 
-    result_list.append(current_string)
-    return result_list
+    result_string = "\n".join(result_list)
+    return result_string
 
 
-input_file = "input_sentences.txt"
+input_file = "input_sentences2.txt"
 output_file = "output_sentences.txt"
 data = read_file(input_file)
-max_length = 12
-processed_data = preprocess_data(data, max_length)
-write_file(output_file, processed_data)
+max_length = 8
+result_string = preprocess_data(data, max_length)
+write_file(output_file, result_string)
