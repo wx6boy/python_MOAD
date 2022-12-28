e4f62653f5df86b6ab3a330220535a6621504e27
wx6boy <a.klenkov52@mail.ru>
2022-12-28 15:43:54 +0300

    seperated on functions

--- practice_exam/practice_task.py
+++ practice_exam/practice_task.py
@@ -10,21 +10,33 @@ def count_vowels(check_word):
     return len([letter for letter in check_word if letter.lower() in 'aeiouауеояиюэ'])
 
 
-def function_process(filename):
+def read_file(filename):
     f = open(filename, "r", encoding="utf-8")
     data = f.read()
     f.close()
+
+    return data
+
+
+def delete_ending_punctuations(word_to_delete):
+    while word_to_delete[-1] in punctuations:
+        word_to_delete = word_to_delete[:-1]
+
+    return word_to_delete
+
+
+def process_data(data):
     data_list = data.split()
     for current_word in data_list:
-        while current_word[-1] in punctuations:
-            current_word = current_word[:-1]
+        current_word = delete_ending_punctuations(current_word)
         count_v = count_vowels(current_word)
         if count_v > 1:
             yield current_word
 
 
 input_file = "input.txt"
-words = function_process(input_file)
+file_data = read_file(input_file)
+words = process_data(file_data)
 for word in words:
     print(word)
 
